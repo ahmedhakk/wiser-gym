860aca060a99145ecf5a2ae233508cb4ede66c2a
ahmed ali <ahmedmohamed302098@gmail.com>
2025-12-14 15:33:45 +0200

    refactor: remove unused GSAP imports from SctackedCards component

--- app/_components/SctackedCards.tsx
+++
@@ -1,6 +0,0 @@
-import { gsap } from "gsap";
-import { useGSAP } from "@gsap/react";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { ScrollSmoother } from "gsap/ScrollSmoother";
-
-gsap.registerPlugin(useGSAP, ScrollTrigger, ScrollSmoother);
